7a603d3f0b80a0545a835056936298b414311889
lizavetayankina <lizavetayankina@gmail.com>
2021-08-01 16:18:01 +0300

    add adap

--- styles/js/script.js
+++ styles/js/script.js
@@ -45,10 +45,47 @@ document.querySelector('.tabs__nav-btn').click();
 
 
 /*slider*/
-const sliderDots = document.querySelectorAll('.slider-dots_item');
-let banner = document.querySelector('.banner');
-for(i of sliderDots) { i.addEventListener('click', () => {
-  banner.style.backg
-})
+const slider = document.querySelector('.slider');
+const dots1 = document.getElementById('slider-dots_item_1');
+const dots2 = document.getElementById('slider-dots_item_2');
+const dots3 = document.getElementById('slider-dots_item_3');
+const dots4 = document.getElementById('slider-dots_item_4');
+const dots5 = document.getElementById('slider-dots_item_5');
 
+let sliderIndex = 1;
+showSlides(sliderIndex);
+
+function plusSlider(n) {
+  showSlides(sliderIndex += n);
+};
+
+function previousSlide(n) {
+  showSlides(slideIndex -= n);  
+}
+
+function currentSlide(n) {
+  showSlides(sliderIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slider = document.querySelectorAll('.item__sl');
+  const dots = document.querySelectorAll('.slider-dots_item');
+
+if (n > slider.length) {
+sliderIndex = 1;
+}
+
+if (n < 1) {
+sliderIndex = slider.length;
+}
+
+for ( i = 0; i < slider.length; i++) {
+slider[i].style.display = "none";
+dots[i].classList.add('active');
+}
+
+
+slider[sliderIndex - 1].style.display='block';
+dots[sliderIndex - 1].className='active';
 }
